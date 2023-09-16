5325ce9ef9d8ec03d7e422968aad67c3ae112e6a
adelaguardiab@gmail.com <adelaguardiab@gmail.com>
2023-09-16 14:33:53 -0400

    making tab as a component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,22 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
-import send from "../../Assets/Icons/send-icon.PNG";
-import receive from "../../Assets/Icons/receive-icon.PNG";
+import send from '../../Assets/Icons/send-icon.PNG'
+import recieve from '../../Assets/Icons/receive-icon.PNG'
+import menu from '../../Assets/Icons/menu.png'
 
 function Header() {
   return (
     <>
-      <div className="Header">
-        <Link to="/scan-send">
-          <img className="Header__send" src={send} alt="scaner to send" />
-        </Link>
-        <Link to="/scan-receive">
-          <img className="Header__receive" src={receive} alt="scaner to send" />
-        </Link>
+    <div className="Tab">
+    <Link to="/scan-send">
+      <div className="Tab__icon-container"><img className="Tab__send"src={recieve} alt="Send" /></div>
+      </Link>
+      <Link to="/menu">
+      <div className="Tab__icon-container"><img className="Tab__menu"src={menu} alt="Menu" /></div>
+      </Link>
+      <Link to="/scan-receive">
+      <div className="Tab__icon-container"><img className="Tab__recieve"src={send} alt="Recieve" /></div>
+      </Link>
       </div>
     </>
   );
--- src/Pages/OnBoardingPage/OnBoardingPage.js
+++ src/Pages/OnBoardingPage/OnBoardingPage.js
@@ -1,16 +1,10 @@
 import "./OnBoardingPage.scss";
-import send from '../../Assets/Icons/send-icon.PNG'
-import recieve from '../../Assets/Icons/receive-icon.PNG'
-import menu from '../../Assets/Icons/menu.png'
+import Header from "../../Components/Header/Header"
 
 function OnBoardingPage() {
   return (
     <>
-      <div className="Tab">
-      <div className="Tab__icon-container"><img className="Tab__send"src={recieve} alt="Send" /></div>
-      <div className="Tab__icon-container"><img className="Tab__menu"src={menu} alt="Menu" /></div>
-      <div className="Tab__icon-container"><img className="Tab__recieve"src={send} alt="Recieve" /></div>
-      </div>
+      <Header />
       <div className="OnBoardingPage"></div>
     </>
   );
