983ac2537312f0457a13d6defa18b0e7da03f19b
adelaguardiab@gmail.com <adelaguardiab@gmail.com>
2023-09-16 14:07:59 -0400

    final push

--- src/Pages/OnBoardingPage/OnBoardingPage.js
+++ src/Pages/OnBoardingPage/OnBoardingPage.js
@@ -1,8 +1,16 @@
 import "./OnBoardingPage.scss";
+import send from '../../Assets/Icons/send-icon.PNG'
+import recieve from '../../Assets/Icons/receive-icon.PNG'
+import menu from '../../Assets/Icons/menu.png'
 
 function OnBoardingPage() {
   return (
     <>
+      <div className="Tab">
+      <div className="Tab__icon-container"><img className="Tab__send"src={recieve} alt="Send" /></div>
+      <div className="Tab__icon-container"><img className="Tab__menu"src={menu} alt="Menu" /></div>
+      <div className="Tab__icon-container"><img className="Tab__recieve"src={send} alt="Recieve" /></div>
+      </div>
       <div className="OnBoardingPage"></div>
     </>
   );
